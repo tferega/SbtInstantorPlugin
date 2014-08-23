fd7953baee64a94026d37bd063b16dab9247a82a
Marko Elezovic <marko@element.hr>
2014-08-23 14:36:22 +0200

    Bumped PropsLoader

--- code/src/main/scala/com/instantor/plugin/Publications.scala
+++ code/src/main/scala/com/instantor/plugin/Publications.scala
@@ -6,13 +6,14 @@ trait Publications {
   val InstantorApi = "com.instantor" % "instantor-api" % "0.4.1"
 
   object PropsLoader {
-    val Api  = "com.instantor.props" % "propsloader-api"  % "0.3.14"
-    val Core = "com.instantor.props" % "propsloader-core" % "0.3.14"
+    val Api  = "com.instantor.props" % "propsloader-api"  % "0.3.16"
+    val Core = "com.instantor.props" % "propsloader-core" % "0.3.16"
   }
 
   object TotaLog {
-    val Interfaces = "com.instantor.totalog" % "totalog-interfaces" % "0.5.1"
-    val Transport  = "com.instantor.totalog" % "totalog-transport"  % "0.5.5"
+    val Interfaces = "com.instantor.totalog" % "totalog-interfaces" % "0.5.6"
+    val Transport  = "com.instantor.totalog" % "totalog-transport"  % "0.5.6"
+    val Search     = "com.instantor.totalog" % "totalog-search"     % "0.5.6"
   }
 
   object BspClientApi {
